2a830e09075ea563d59e066ac8c8429811d70142
AzharM0116 <62126452+AzharM0116@users.noreply.github.com>
2024-06-06 17:26:21 +0530

    Added Deployment Script.sql file

---
+++ Migration/Deployment_Script_SRT01.sql
@@ -0,0 +1,13 @@
+-- Author : Azhar
+-- Date(DD-MM-YYYY) : 04-May-2024
+-- Object : 
+-- Description : This file contains the list of sql files to deploy with .sql extension
+-- Other Comments : 
+-- SRT reference for Change : 
+
+
+Table1.SQL
+
+Table2.SQL
+
+SELECT 'Main script executed' AS status;
