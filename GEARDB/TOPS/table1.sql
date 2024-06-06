2a6fd733617a728d39e69f681d217328320269c8
AzharM0116 <62126452+AzharM0116@users.noreply.github.com>
2024-06-06 16:53:25 +0530

    Added Initial Files for Table1.sql and Table2.sql

---
+++ GEARDB/TOPS/table1.sql
@@ -0,0 +1,9 @@
+-- Author : Azhar
+-- Date(DD-MM-YYYY) : 04-May-2024
+-- Object : 
+-- Description : This file contains the Description of 
+-- Other Comments : 
+-- SRT reference for Change : 
+
+CREATE TABLE table1 ( id INTEGER, name STRING );
+
---
+++ GEARDB/TOPS/table2.sql
@@ -0,0 +1,9 @@
+-- Author : Azhar
+-- Date(DD-MM-YYYY) : 04-May-2024
+-- Object : table2.sql
+-- Description : This file contains the Description of 
+-- Other Comments : 
+-- SRT reference for Change : 
+
+CREATE TABLE table2 ( id INTEGER, description STRING );
+
